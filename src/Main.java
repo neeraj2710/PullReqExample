796b8b9236b887b0d81382393413e17dbf102f2b
neera <neerajwadhwaney2003@gmail.com>
2025-11-28 13:57:58 +0530

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     static void main() {
         System.out.println("Hello World");
-        System.out.println("this is neeraj wadhwaney");
+        System.out.println("this is neeraj wadhwaney from main branch");
         System.out.println("Welcome to Java SE");
     }
 }
